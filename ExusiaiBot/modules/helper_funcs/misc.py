33d280130ce2b4540580a1586641d7dec33d5f9b
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-07-14 17:35:54 +0530

    Update misc.py

--- ExusiaiBot/modules/helper_funcs/misc.py
+++ ExusiaiBot/modules/helper_funcs/misc.py
@@ -160,3 +160,8 @@ def user_bot_owner(func):
             pass
 
     return is_user_bot_owner
+
+def sendMessage(text: str, bot: Bot, update: Update):
+    return bot.send_message(update.message.chat_id,
+                                    reply_to_message_id=update.message.message_id,
+                                    text=text, parse_mode=ParseMode.HTML)
